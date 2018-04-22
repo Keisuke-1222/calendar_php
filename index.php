d3295b9ec4f75ba86f6c94579b3599e56431b03c
vagrant <vagrant@localhost.localdomain>
2018-04-22 15:21:16 +0900

    add link to next/prev month

--- index.php
+++ index.php
@@ -1,22 +1,53 @@
 <?php
 
+function h($s) {
+  return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+}
+
+try {
+  if (!isset($_GET['t']) || !preg_match('/\A\d{4}-\d{2}\z/', $_GET['t'])) {
+    throw new Exception();
+  }
+  $thisMonth = new DateTime($_GET['t']);
+} catch (Exception $e) {
+  $thisMonth = new DateTime('first day of this month');
+}
+
+$dt = clone $thisMonth;
+$prev = $dt->modify('-1 month')->format('Y-m');
+$dt = clone $thisMonth;
+$next = $dt->modify('+1 month')->format('Y-m');
+
+$yearMonth = $thisMonth->format('F Y');
+
+$tail = '';
+$lastDayOfPrevMonth = new DateTime('last day of ' . $yearMonth . ' -1 month');
+while ($lastDayOfPrevMonth->format('w') < 6) {
+  $tail = sprintf('<td class="gray">%d</td>', $lastDayOfPrevMonth->format('d')) . $tail;
+  $lastDayOfPrevMonth->sub(new DateInterval('P1D'));
+}
+
 $body = '';
 $period = new DatePeriod(
-  new DateTime('first day of this month'),
+  new DateTime('first day of ' . $yearMonth),
   new DateInterval('P1D'),
-  new DateTime('first day of next month')
+  new DateTime('first day of ' . $yearMonth . ' +1 month')
 );
+$today = new DateTime('today');
 foreach ($period as $day) {
   if ($day->format('w') % 7 === 0) { $body .= '</tr><tr>'; }
-  $body .= sprintf('<td class="youbi_%d">%d</td>', $day->format('w'), $day->format('d'));
+  $todayClass = ($day->format('Y-m-d') === $today->format('Y-m-d')) ? 'today' : '';
+  $body .= sprintf('<td class="youbi_%d %s">%d</td>', $day->format('w'), $todayClass, $day->format('d'));
 }
 
 $head = '';
-$firstDayOfNextMonth = new DateTime('first day of next month');
+$firstDayOfNextMonth = new DateTime('first day of ' . $yearMonth . ' +1 month');
 while ($firstDayOfNextMonth->format('w') > 0) {
   $head .= sprintf('<td class="gray">%d</td>', $firstDayOfNextMonth->format('d'));
   $firstDayOfNextMonth->add(new DateInterval('P1D'));
 }
+
+$html = '<tr>' . $tail . $body . $head . '</tr>';
 
 ?>
 <!DOCTYPE html>
@@ -30,9 +61,9 @@ while ($firstDayOfNextMonth->format('w') > 0) {
   <table>
     <thead>
       <tr>
-        <th><a href="">&laquo;</a></th>
-        <th colspan="5">August 2015</th>
-        <th><a href="">&raquo;</a></th>
+        <th><a href="/?t=<?php echo h($prev); ?>">&laquo;</a></th>
+        <th colspan="5"><?php echo h($yearMonth); ?></th>
+        <th><a href="/?t=<?php echo h($next); ?>">&raquo;</a></th>
       </tr>
     </thead>
     <tbody>
@@ -45,29 +76,11 @@ while ($firstDayOfNextMonth->format('w') > 0) {
         <td>Fri</td>
         <td>Sat</td>
       </tr>
-      <tr>
-        <?php echo $body . $head; ?>
-        <!-- <td class="youbi_0">1</td>
-        <td class="youbi_1">2</td>
-        <td class="youbi_2">3</td>
-        <td class="youbi_3">4</td>
-        <td class="youbi_4 today">5</td>
-        <td class="youbi_5">6</td>
-        <td class="youbi_6">8</td>
-      </tr>
-      <tr>
-        <td class="youbi_0">30</td>
-        <td class="youbi_1">31</td>
-        <td class="gray">1</td>
-        <td class="gray">2</td>
-        <td class="gray">3</td>
-        <td class="gray">4</td>
-        <td class="gray">5</td> -->
-      </tr>
+      <?php echo $html; ?>
     </tbody>
     <tfoot>
       <tr>
-        <th colspan="7"><a href="">Today</a></th>
+        <th colspan="7"><a href="/">Today</a></th>
       </tr>
     </tfoot>
   </table>
